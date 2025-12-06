5985f5a729478018589406cbd66c1686edfd0fc1
Cleiver Mejia <CleiverMejiaR@gmail.com>
2025-12-06 09:44:12 -0500

    Update .gitignore, add MANIFEST file, refactor Sprite class, and enhance Ski class functionality

--- src/util/Sprite.java
+++ src/util/Sprite.java
@@ -1,44 +1,60 @@
 package util;
 
-import enums.Action;
 import java.awt.Image;
-import java.util.HashMap;
 import java.util.Map;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
+
+import enums.Action;
 
 public class Sprite {
 
-    private final String URL_BASE = "src/assets/Ski_";
-    private final int SCALE = 4;
+    private static final String URL_BASE = "src/assets/Ski_";
+    private static final int SCALE = 4;
 
-    Map<String, String> sprNames = Map.of(
-            "idle", "idle.png",
-            "walk", "walk.gif",
-            "run", "run.gif",
-            "jump", "jump.png",
-            "drag", "drag.png",
-            "falling", "jump.png",
-            "down", "down.png",
-            "up", "up.png",
-            "sit", "sit.png"
+    private static final ImageIcon idleIcon = new ImageIcon(URL_BASE + "idle.png");
+    private static final ImageIcon walkIcon = new ImageIcon(URL_BASE + "walk.gif");
+    private static final ImageIcon runIcon = new ImageIcon(URL_BASE + "run.gif");
+    private static final ImageIcon jumpIcon = new ImageIcon(URL_BASE + "jump.png");
+    private static final ImageIcon dragIcon = new ImageIcon(URL_BASE + "drag.png");
+    private static final ImageIcon fallingIcon = new ImageIcon(URL_BASE + "jump.png");
+    private static final ImageIcon upIcon = new ImageIcon(URL_BASE + "up.png");
+    private static final ImageIcon downIcon = new ImageIcon(URL_BASE + "down.png");
+    private static final ImageIcon sitIcon = new ImageIcon(URL_BASE + "sit.png");
+
+    private static final Image idleImage = idleIcon.getImage().getScaledInstance(idleIcon.getIconWidth() * SCALE, idleIcon.getIconHeight() * SCALE, Image.SCALE_SMOOTH);
+    private static final Image walkImage = walkIcon.getImage().getScaledInstance(walkIcon.getIconWidth() * SCALE, walkIcon.getIconHeight() * SCALE, Image.SCALE_DEFAULT);
+    private static final Image runImage = runIcon.getImage().getScaledInstance(runIcon.getIconWidth() * SCALE, runIcon.getIconHeight() * SCALE, Image.SCALE_DEFAULT);
+    private static final Image jumpImage = jumpIcon.getImage().getScaledInstance(jumpIcon.getIconWidth() * SCALE, jumpIcon.getIconHeight() * SCALE, Image.SCALE_SMOOTH);
+    private static final Image dragImage = dragIcon.getImage().getScaledInstance(dragIcon.getIconWidth() * SCALE, dragIcon.getIconHeight() * SCALE, Image.SCALE_SMOOTH);
+    private static final Image fallingImage = fallingIcon.getImage().getScaledInstance(fallingIcon.getIconWidth() * SCALE, fallingIcon.getIconHeight() * SCALE, Image.SCALE_SMOOTH);
+    private static final Image upImage = upIcon.getImage().getScaledInstance(upIcon.getIconWidth() * SCALE, upIcon.getIconHeight() * SCALE, Image.SCALE_SMOOTH);
+    private static final Image downImage = downIcon.getImage().getScaledInstance(downIcon.getIconWidth() * SCALE, downIcon.getIconHeight() * SCALE, Image.SCALE_SMOOTH);
+    private static final Image sitImage = sitIcon.getImage().getScaledInstance(sitIcon.getIconWidth() * SCALE, sitIcon.getIconHeight() * SCALE, Image.SCALE_SMOOTH);
+
+    private static final Icon idle = new ImageIcon(idleImage);
+    private static final Icon walk = new ImageIcon(walkImage);
+    private static final Icon run = new ImageIcon(runImage);
+    private static final Icon jump = new ImageIcon(jumpImage);
+    private static final Icon drag = new ImageIcon(dragImage);
+    private static final Icon falling = new ImageIcon(fallingImage);
+    private static final Icon up = new ImageIcon(upImage);
+    private static final Icon down = new ImageIcon(downImage);
+    private static final Icon sit = new ImageIcon(sitImage);
+
+    private static final Map<Action, Icon> sprImages = Map.of(
+            Action.IDLE, idle,
+            Action.WALK, walk,
+            Action.RUN, run,
+            Action.JUMP, jump,
+            Action.DRAG, drag,
+            Action.FALLING, falling,
+            Action.DOWN, down,
+            Action.UP, up,
+            Action.SIT, sit
     );
 
-    private final Map<Action, ImageIcon> spriteImages = new HashMap<>();
-
-    public Sprite() {
-        for (Action action : Action.values()) {
-            String sprName = action.name().toLowerCase();
-
-            ImageIcon icon = new ImageIcon(URL_BASE + sprName + ".png");
-            Image iconBig = icon.getImage().getScaledInstance(
-                    icon.getIconWidth() * SCALE, icon.getIconHeight() * SCALE, Image.SCALE_SMOOTH
-            );
-
-            spriteImages.put(action, new ImageIcon(iconBig));
-        }
-    }
-
-    public ImageIcon get(Action action) {
-        return spriteImages.get(action);
+    public static Icon get(Action action) {
+        return sprImages.get(action);
     }
 }
--- src/view/Ski.java
+++ src/view/Ski.java
@@ -1,6 +1,5 @@
 package view;
 
-import enums.Action;
 import java.awt.Color;
 import java.awt.MouseInfo;
 import java.awt.Point;
@@ -17,6 +16,8 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 import javax.swing.Timer;
+
+import enums.Action;
 import util.Sprite;
 
 public class Ski extends JLabel {
@@ -26,18 +27,17 @@ public class Ski extends JLabel {
     private int hLim = 0;
     private float y = 0;
     private float vSpeed = 0;
-    private float vLim = 10;
+    private final float vLim = 10;
     private int width = 0;
     private int height = 0;
     private int direction = 1;
     private int randomXPosition = 0;
 
     private Action action = Action.FALLING;
-    private Sprite sprite = new Sprite();
 
     public Ski() {
-        //this.setBackground(new Color(0, 0, 0, 0));
-        this.setBackground(Color.RED);
+        this.setBackground(new Color(0, 0, 0, 0));
+        //this.setBackground(Color.RED);
         this.setOpaque(true);
         setSprite();
 
@@ -65,113 +65,12 @@ public class Ski extends JLabel {
             }
         }));
 
-        new Timer(10, (ActionEvent e) -> { // Repeticion
-            //new step();
-            switch (action) {
-                case WALK, RUN -> {
-                    if (hSpeed < hLim) {
-                        hSpeed += .5f;
-                    }
-
-                    for (int i = 0; i < hSpeed; i++) {
-                        if (Math.signum(randomXPosition - x + direction) != 0) {
-                            x += direction;
-                            continue;
-                        }
-
-                        action = Action.IDLE;
-                        setSprite();
-                        break;
-                    }
-                }
-                case JUMP -> {
-                    if (vSpeed < 0) {
-                        y += vSpeed;
-                        vSpeed += .2f;
-
-                        break;
-                    }
-
-                    action = Action.FALLING;
-                    setSprite();
-                }
-                case FALLING -> {
-                    if (vSpeed < vLim) {
-                        vSpeed += .2f;
-                    }
-
-                    for (int i = 0; i < vSpeed; i++) {
-                        if (y < Main.SCREEN_HEIGHT - height) {
-                            y++;
-                            continue;
-                        }
-
-                        vSpeed = 0;
-                        y = Main.SCREEN_HEIGHT - height;
-                        action = Action.IDLE;
-                        setSprite();
-                    }
-                }
-                case DRAG -> {
-                }
-                default -> {
-                    if (hSpeed < 0) {
-                        hSpeed = 0;
-                        break;
-                    }
-
-                    hSpeed -= .2f;
-                    x += (int) (direction * hSpeed);
-                }
-            }
-
-            this.setBounds((int) x, (int) y, width, height);
-        }).start();
-
-        AtomicInteger timeRandom = new AtomicInteger(10000);
-        new Timer(timeRandom.get(), (ActionEvent e) -> {
-            int rNumber = (int) (Math.random() * 7);
-            Action actionSelected = Action.values()[rNumber];
-
-            if (action == Action.WALK || action == Action.RUN || action == Action.DRAG || action == Action.FALLING) {
-                return;
-            }
-
-            action = actionSelected;
-
-            switch (action) {
-                case IDLE ->
-                    setSprite();
-                case WALK, RUN -> {
-                    randomXPosition = (int) (Main.SCREEN_WIDTH * Math.random());
-                    direction = (int) Math.signum(randomXPosition - x);
-                    hLim = actionSelected == Action.WALK ? 3 : 7;
-
-                    setSprite();
-                }
-                case JUMP -> {
-                    vSpeed = -8;
-                    setSprite();
-                }
-                case SIT ->
-                    setSprite();
-                case UP ->
-                    setSprite();
-                case DOWN ->
-                    setSprite();
-                default -> {
-                }
-            }
-
-            timeRandom.set((int) (Math.random() * 10000));
-            ((Timer) e.getSource()).setDelay(timeRandom.get());
-
-            System.out.println(actionSelected);
-        }).start();
+        initThreads();
     }
 
-    private class step {
-
+    private void initThreads() {
+        new Thread(new Step()).start();
+        new Thread(new ActionSelect()).start();
     }
 
     private Icon flipHorizontal(Icon icon) {
@@ -183,7 +82,6 @@ public class Ski extends JLabel {
 
         AffineTransform tx = AffineTransform.getScaleInstance(-1, 1);
         tx.translate(-w, 0);
-        //tx.rotate(Math.toRadians(rotate), w/2f, h/2f);
 
         AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
         BufferedImage flipped = op.filter(img, null);
@@ -192,7 +90,7 @@ public class Ski extends JLabel {
     }
 
     private void setSprite() {
-        Icon icon = sprite.get(action);
+        Icon icon = Sprite.get(action);
 
         if (this.direction == -1) {
             icon = flipHorizontal(icon);
@@ -202,5 +100,112 @@ public class Ski extends JLabel {
         this.y += this.height - icon.getIconHeight();
         this.width = icon.getIconWidth();
         this.height = icon.getIconHeight();
+    }
+
+    private class Step extends Thread {
+
+        @Override
+        public void run() {
+            new Timer(10, (ActionEvent e) -> {
+                switch (action) {
+                    case WALK, RUN -> {
+                        if (hSpeed < hLim) {
+                            hSpeed += .5f;
+                        }
+
+                        for (int i = 0; i < hSpeed; i++) {
+                            if (Math.signum(randomXPosition - x + direction) != 0) {
+                                x += direction;
+                                continue;
+                            }
+
+                            action = Action.IDLE;
+                            setSprite();
+                            break;
+                        }
+                    }
+                    case JUMP -> {
+                        if (vSpeed < 0) {
+                            y += vSpeed;
+                            vSpeed += .2f;
+
+                            break;
+                        }
+
+                        action = Action.FALLING;
+                        setSprite();
+                    }
+                    case FALLING -> {
+                        if (vSpeed < vLim) {
+                            vSpeed += .2f;
+                        }
+
+                        for (int i = 0; i < vSpeed; i++) {
+                            if (y < Main.SCREEN_HEIGHT - height) {
+                                y++;
+                                continue;
+                            }
+
+                            vSpeed = 0;
+                            y = Main.SCREEN_HEIGHT - height;
+                            action = Action.IDLE;
+                            setSprite();
+                        }
+                    }
+                    default -> {
+                        if (hSpeed < 0) {
+                            hSpeed = 0;
+                            break;
+                        }
+
+                        hSpeed -= .2f;
+                        x += (int) (direction * hSpeed);
+                    }
+                }
+
+                setBounds((int) x, (int) y, width, height);
+            }).start();
+        }
+    }
+
+    private class ActionSelect extends Thread {
+
+        @Override
+        public void run() {
+            AtomicInteger timeRandom = new AtomicInteger(10000);
+            new Timer(timeRandom.get(), (ActionEvent e) -> {
+                int rNumber = (int) (Math.random() * 7);
+                Action actionSelected = Action.values()[rNumber];
+
+                if (action == Action.WALK
+                        || action == Action.RUN
+                        || action == Action.DRAG
+                        || action == Action.FALLING
+                        || action == Action.JUMP) {
+                    return;
+                }
+
+                action = actionSelected;
+
+                switch (action) {
+                    case WALK, RUN -> {
+                        randomXPosition = (int) (Main.SCREEN_WIDTH * Math.random());
+                        direction = (int) Math.signum(randomXPosition - x);
+                        hLim = actionSelected == Action.WALK ? 3 : 7;
+                    }
+                    case JUMP ->
+                        vSpeed = -8;
+                    default -> {
+                    }
+                }
+
+                setSprite();
+
+                timeRandom.set((int) (Math.random() * 10000));
+                ((Timer) e.getSource()).setDelay(timeRandom.get());
+
+                System.out.println(actionSelected);
+            }).start();
+        }
     }
 }
